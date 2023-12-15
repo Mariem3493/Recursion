71a25190d8a49b7411dbc6d098d44dff61b3279d
Mariem OUECHTATI (SKR-09-GMSCM) <Mariem.OUECHTATI@asteelflash.com>
2023-12-15 14:06:37 +0100

    Recursion

---
+++ function palindrome(str) {.js
@@ -0,0 +1,13 @@
+// Vérification si un mot se lit de gauche à droite ou pas
+function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
+palindrome("A man, a plan, a canal. Panama");
